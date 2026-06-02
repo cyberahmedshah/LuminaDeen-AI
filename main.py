4fc5e24cfdf49322d8d09ee7fb905c3bb550c1fd
ahmedshah <cyberahmedshah@gmail.com>
2026-06-03 02:52:38 +0500

    added key for hardcode question

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 with open("mind.json", "r") as f:
     data = json.load(f)
-    question = input("Ask Question: ")
+    question = input("Ask Question: ").lower()
     if "name" in question:
      print(f"Answer: {data['name']}")
+    if "purpose" in question:
+       print(f"Answer: {data['purpose']}")
