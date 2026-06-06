5947f5b5fce6b6cdacaf3f69c5c6f667ec50f68d
ahmedshah <cyberahmedshah@gmail.com>
2026-06-07 04:38:40 +0500

    Connected frontend and backend

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import json
 
 app = Flask(__name__)
 
+# load mind.json once when app starts
+with open("mind.json", "r") as f:
+    data = json.load(f)
+
 @app.route('/')
-def hello():
+def home():
     return render_template('index.html')
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    question = request.json.get('question').lower()  # gets question from browser
+    answer = "I don't know the answer to that."      # default answer
+
+    if "name" in question:
+        answer = data['name']
+    if "purpose" in question:
+        answer = data['purpose']
+    if "hey" in question:
+        answer = data['hey']
+
+    return jsonify({'answer': answer})
 
 if __name__ == '__main__':
     app.run(debug=True)
