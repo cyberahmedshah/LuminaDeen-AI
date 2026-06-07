33e9b8735c8e6dbe7835886658c55a5c087ba4fa
ahmedshah <cyberahmedshah@gmail.com>
2026-06-07 05:05:11 +0500

    Deleted main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import json
-with open("mind.json", "r") as f:
-    data = json.load(f)
-    question = input("Ask Question: ").lower()
-    if "name" in question:
-     print(f"Answer: {data['name']}")
-    if "purpose" in question:
-       print(f"Answer: {data['purpose']}")
