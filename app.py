3ba3f8198edcd44ca83d1b37ad2b3f57fbd59e90
ahmedshah <cyberahmedshah@gmail.com>
2026-06-20 15:51:49 +0500

    fixed ping route

--- app.py
+++ app.py
@@ -89,9 +89,10 @@ def ask():
     
     return jsonify({'answer': answer})
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
 @app.route('/ping')
 def ping():
     return 'OK', 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
